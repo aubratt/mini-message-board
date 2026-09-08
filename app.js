001b53c9494be15880427d339d62b9fa0eb3d358
Austin <ausbratton@gmail.com>
2026-09-08 11:59:27 -0500

    refactor route handling

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
@@ -17,9 +20,6 @@ app.listen(PORT, (error) => {
 
 const links = [{ href: "/" }];
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
 app.post("/new", (req, res) => {});
 
 app.use((error, req, res, next) => {
