ba85256849f249139f53004c725c9e8c9ccf070f
Austin <ausbratton@gmail.com>
2026-09-08 12:21:34 -0500

    add content to index view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -14,6 +14,6 @@ const messages = [
   },
 ];
 
-indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/", (req, res) => res.render("index", { messages: messages }));
 
 module.exports = indexRouter;
