6ac004246c64e3e2adf8bbe7e285b8e055e73825
Austin <ausbratton@gmail.com>
2026-09-08 13:01:16 -0500

    new message form posts to index

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -17,7 +17,12 @@ const messages = [
 indexRouter.get("/", (req, res) => res.render("index", { messages: messages }));
 indexRouter.get("/new", (req, res) => res.render("form"));
 indexRouter.post("/new", (req, res) => {
-  messages.push({ text: req.message, user: req.user, added: new Date() });
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = indexRouter;
